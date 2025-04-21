5ab2269244bfc546ef07c3d8e41efb002311d1fd
James Pederson <james@jpederson.com>
2025-04-21 20:05:48 +0000

    Add basic components and one and two column content components.

--- library/component/accordions.php
+++ library/component/accordions.php
@@ -1,16 +1,20 @@
-<?php while ( have_rows( 'accordions' ) ) : the_row(); 
-    $cutout = get_sub_field( 'cutout' ); ?>
-<div class="accordion<?php print ( get_sub_field('open') ? ' open' : '' ); ?>">
-    <div class="accordion-handle <?php the_sub_field( 'color' ); ?>">
-        <h3><?php the_sub_field( 'title' ); ?></h3>
-    </div>
-    <div class="accordion-content<?php if ( !empty( $cutout ) ) { print " has-cutout"; } ?>">
-        <div class="accordion-content-inner">
-            <?php the_sub_field( 'content' ); ?>
+<?php if ( have_rows( 'accordions' ) ) : ?>
+<div class="accordion-container">
+    <?php while ( have_rows( 'accordions' ) ) : the_row(); 
+        $cutout = get_sub_field( 'cutout' ); ?>
+    <div class="accordion<?php print ( get_sub_field('open') ? ' open' : '' ); ?>">
+        <div class="accordion-handle <?php the_sub_field( 'color' ); ?>">
+            <h3><?php the_sub_field( 'title' ); ?></h3>
         </div>
-        <?php if ( !empty( $cutout ) ) : ?>
-        <div class="accordion-cutout" style="background-image: url(<?php print $cutout ?>);"></div>
-        <?php endif; ?>
+        <div class="accordion-content<?php if ( !empty( $cutout ) ) { print " has-cutout"; } ?>">
+            <div class="accordion-content-inner">
+                <?php the_sub_field( 'content' ); ?>
+            </div>
+            <?php if ( !empty( $cutout ) ) : ?>
+            <div class="accordion-cutout" style="background-image: url(<?php print $cutout ?>);"></div>
+            <?php endif; ?>
+        </div>
     </div>
+    <?php endwhile; ?>
 </div>
-<?php endwhile; ?>
+<?php endif; ?>
