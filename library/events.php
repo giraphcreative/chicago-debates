67b70d2c5336b6a58c0fe156ab82b8a1ee6cd11e
James Pederson <james@jpederson.com>
2025-09-16 01:01:05 +0000

    Hide past events on main events listing.

--- library/events.php
+++ library/events.php
@@ -8,13 +8,13 @@ function event_adjust_queries($query){
         $query->set( 'orderby', 'meta_value' );
         $query->set( 'order', 'ASC' );
         
-        /*$query->set( 'meta_query', array(
+        $query->set( 'meta_query', array(
             array(
                 'key' => 'start',
                 'value' => $today,
                 'compare' => '>'
             ),
-        ));*/
+        ));
     }
 }
 add_action( 'pre_get_posts', 'event_adjust_queries' );
